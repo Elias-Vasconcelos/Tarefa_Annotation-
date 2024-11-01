9be988940145afc1b18f65e3d1bde0897720c0cb
Elias-Vasconcelos <146135801+Elias-Vasconcelos@users.noreply.github.com>
2024-11-01 18:19:33 -0300

    criacao e açociacao da anotacao

---
+++ annotation/src/Main.java
@@ -0,0 +1,15 @@
+import anotacao.TabelaAnnotation;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    @TabelaAnnotation(value = "tabela de valores")
+    String tabelaDeValores;
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
